793839d8c77e8c7162b1281b9271aa617269aff3
eridev14 <eridev14hc@gmail.com>
2023-08-01 11:16:46 -0400

    4 ejer hack

--- algoritmosHackerRank/01calentamiento/diagonal-difference.js
+++ algoritmosHackerRank/01calentamiento/diagonal-difference.js
@@ -1,14 +1,23 @@
 function diagonalDifference(arr) {
-    // Write your code here
+    let a = 0;
+    let b = arr.length - 1;
+    let sumDiag1 = 0;
+    let sumDiag2 = 0;
 
-    
-    return arr[2][0]
+    arr.forEach((el, i) => {
+        sumDiag1 += el[a];
+        sumDiag2 += el[b];
+        a++;
+        b--;
+    });
+
+    return Math.abs(sumDiag1 - sumDiag2)
 }
 
 let matriz = [
-    [1, 2, 5],
-    [3, 2, 1],
-    [7, 1, 5]
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9]
 ];
 
 console.log(diagonalDifference(matriz));
--- desafio-30-dias-HackerRank/dia01.js
+++
@@ -1,6 +0,0 @@
-function processData(inputString) {
-    console.log("Hello, World.");
-    console.log(inputString);
-}
-
-processData("Welcome to 30 Days of Code!")
