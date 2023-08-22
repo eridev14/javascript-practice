31e964fd63100addd97fd091ddaf55769b7a4821
eridev14 <eridev14hc@gmail.com>
2023-08-22 10:36:21 -0400

    subarray division

---
+++ algoritmosHackerRank/02implementacion/subarray-division.js
@@ -0,0 +1,23 @@
+function birthday(s, d, m) {
+    if (!(1 <= s.length <= 100)) return;
+    if (!s.every(val => val >= 1 && val <= 5)) return
+    if (!(1 <= d <= 31)) return
+    if (!(1 <= m <= 12)) return
+
+    let cont = 0;
+    for (let i = 0; i < s.length - m + 1; i++) {
+        let el = 0;
+        for (let j = 0; j < m; j++) {
+            el += s[i + j];
+        }
+
+        if (el === d) {
+            cont++
+        }
+    }
+    return cont;
+}
+
+let s = [1, 2, 1, 3, 2]
+console.log(birthday(s, 3, 2));
+
