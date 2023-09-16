e51bce54fd3ffa25b84dee7d08802a3ec2f955f9
eridev14 <eridev14hc@gmail.com>
2023-09-16 17:12:24 -0400

    mod counting valleys

--- algoritmosHackerRank/02implementacion/counting-valleys.js
+++ algoritmosHackerRank/02implementacion/counting-valleys.js
@@ -17,15 +17,34 @@ function countingValleys(steps, path) {
             stepPath.step--
         }
 
-        console.log(stepPath.step);
-        console.log(stepPath.previousStep);
-
-        if (stepPath.step === 0 && (stepPath.previousStep === -1)) {
+        if (stepPath.step === 0 && stepPath.previousStep === -1) {
             valleys++;
         }
     }
-    
+
+    return valleys;
+}
+
+function countingValleysGPT(steps, path) {
+    let valleys = 0;
+    let altitude = 0;
+
+    for (let i = 0; i < steps; i++) {
+        if (path[i] === "U") {
+            altitude++;
+        } else if (path[i] === "D") {
+            altitude--;
+        }
+
+        if (path[i] === "U" && altitude === 0) {
+            valleys++;
+        }
+    }
+
     return valleys;
 }
 
 console.log(countingValleys(12, "DDUUDDUDUUUD"));
+
+
+console.log(countingValleysGPT(12, "DDUUDDUDUUUD"));
