1bdc2d6054f183f53f28f178cfd8d5cc810b88da
eridev14 <eridev14hc@gmail.com>
2024-03-07 09:23:27 -0400

    2d array mod

--- estructuraDatosHackerRank/arrays/02-2d-array.js
+++ estructuraDatosHackerRank/arrays/02-2d-array.js
@@ -40,3 +40,29 @@ const matriz = [
 
 console.log(hourglassSum(matriz));
 
+//-------------
+//otra solucion mas optimizada
+
+function hourglassSumO(arr) {
+    let may = arr[0][0] + arr[0][1] + arr[0][2] +
+        arr[1][1] +
+        arr[2][0] + arr[2][1] + arr[2][2];
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = 0; j < arr.length - 2; j++) {
+            let sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                arr[i + 1][j + 1] +
+                arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+            if (sum > may) {
+                may = sum;
+            }
+            console.log(sum);
+        }
+    }
+
+    return may;
+}
+
+//-------------
+
