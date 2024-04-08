c0054029e0165e62c741f876a649aff061793c5e
eridev14 <eridev14hc@gmail.com>
2024-04-08 09:56:17 -0400

    next permutation add function

---
+++ algoritmosHackerRank/02-implementacion/medium/bigger-is-greater.js
@@ -0,0 +1,40 @@
+function biggerIsGreater(w) {
+    let newArr = w.split('');
+    if (nextPermutation(newArr)) {
+        let neww = newArr.join('');
+        console.log(neww);
+    }
+}
+
+function nextPermutation(array) {
+    // Encuentra el índice más grande i tal que array[i] < array[i + 1]
+    let i = array.length - 2;
+    while (i >= 0 && array[i] >= array[i + 1]) {
+        i--;
+    }
+
+    // Si no hay tal índice, la permutación es la última posible
+    if (i === -1) return false;
+
+    // Encuentra el índice más grande j tal que array[j] > array[i]
+    let j = array.length - 1;
+    while (array[j] <= array[i]) {
+        j--;
+    }
+
+    // Intercambia array[i] y array[j]
+    [array[i], array[j]] = [array[j], array[i]];
+
+    // Invierte la porción de array después de i
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(biggerIsGreater("abcd"));
