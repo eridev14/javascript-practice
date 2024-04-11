483020094db5f515b7c29b4059a8c49376d78efe
eridev14 <eridev14hc@gmail.com>
2024-04-11 11:16:24 -0400

    bigger is greater

--- algoritmosHackerRank/02-implementacion/medium/bigger-is-greater.js
+++ algoritmosHackerRank/02-implementacion/medium/bigger-is-greater.js
@@ -1,35 +1,27 @@
 function biggerIsGreater(w) {
-    w = w.split(""); // Convertir la cadena en un arreglo de caracteres
+    w = w.split("");
 
-    // Encontrar el índice del primer carácter que no sigue la secuencia descendente
     let i = w.length - 2;
     while (i >= 0 && w[i] >= w[i + 1]) {
         i--;
     }
 
-    // Si i es -1, significa que la cadena está en orden descendente y no hay permutaciones más grandes posibles
-    if (i == -1) {
-        return "no answer";
-    }
+    if (i == -1) return "no answer";
 
-    // Encontrar el índice del siguiente carácter más grande que el carácter en la posición i
     let j = w.length - 1;
     while (w[j] <= w[i]) {
         j--;
     }
 
-    // Intercambiar los caracteres en las posiciones i y j
     [w[i], w[j]] = [w[j], w[i]];
 
-    console.log(w);
-
-    // Revertir la parte de la cadena después de la posición i
-    w = w.slice(0, i + 1).concat(w.slice(i + 1).reverse());
-
-    // Unir el arreglo de caracteres para formar la cadena final
+    w = [...w.slice(0, i + 1), ...w.slice(i + 1).reverse()];
     return w.join("");
 }
 
-// Ejemplo de uso:
-let word = "dkhc";
-console.log(biggerIsGreater(word));  // Salida esperada: "acbd"
+let word = "1432";
+console.log(biggerIsGreater(word));
+
+
+
+
